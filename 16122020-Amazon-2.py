4abfc2223b11cc0704051c5c03a4439cc337ad62
Long Le <l.le@gooesoft.com>
2020-12-16 16:53:22 +1100

    init 16/12

--- 16122020-Amazon-2.py
+++ 16122020-Amazon-2.py
@@ -5,3 +5,13 @@
 
 # For example, given s = "abcba" and k = 2, the longest substring with k distinct
 # characters is "bcb".
+
+def longest_substring(s,k):
+    return 0
+
+def main():
+    print('Hello this is a problem from Amazon on 16/12/2020')
+
+if __name__ == '__main__':
+    main()
+
