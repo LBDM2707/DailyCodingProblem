a5adf96161e84392796f07e3d9cb163d8d8e7834
Long Le <l.le@gooesoft.com>
2020-12-23 11:15:07 +1100

    add 21 and 23

--- 18122020-Facebook-2.py
+++ 18122020-Facebook-2.py
@@ -3,7 +3,8 @@
 # Given a stream of elements too large to store in memory, pick a random element 
 # from the stream with uniform probability.
 
-def get_random(n)
+def get_random(n):
+    return 0
 
 def main():
     print("18/12/2020, weird Facebook problem")
--- 22122020-Facebook.py
+++ 22122020-Facebook.py
@@ -7,3 +7,36 @@
 # Given an N by K matrix where the nth row and kth column represents the cost to 
 # build the nth house with kth color, return the minimum cost which achieves this
 # goal.
+
+min = None
+# Back tracking + prunning
+def min_cost(matrix, house=-1, color=0, sum=0):
+    global min
+    # stop condition house == len matrix
+    if house == len(matrix)-1:
+        if min == None:
+            min = sum
+        else:
+            min = sum if sum <= min else min
+    else:
+        # prunning
+        if (min != None and sum <= min) or (min == None):
+            curr = 0
+            if house >= 0:
+                curr = matrix[house][color]
+            for i in range(len(matrix[house])):
+                if i != color:
+                    min_cost(matrix, house + 1, i, sum + curr)
+    
+
+n = 5
+k = 4
+test = [
+    [1,6,4,2],
+    [5,3,4,15],
+    [15,2,7,3],
+    [4,8,5,12],
+    [4,1,2,25],
+]
+min_cost(test)
+print (min)
