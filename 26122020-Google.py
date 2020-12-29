bf33e8ce5dc085c3a70b67cd35a1c0fb8c724085
Long Le <l.le@gooesoft.com>
2020-12-29 14:57:15 +1100

    solved 26

--- 26122020-Google.py
+++ 26122020-Google.py
@@ -19,3 +19,46 @@
 # and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum 
 # number of steps required to reach the end is 7, since we would need to go 
 # through (1, 2) because there is a wall everywhere else on the second row.
+maze = [
+    [0, 0, 0, 0],
+    [1, 1, 0, 1],
+    [0, 0, 0, 0],
+    [1, 1, 1, 0]
+]
+min = None
+
+def valid_move(maze, point):
+    x = point[0]
+    y = point[1]
+    m = len(maze)
+    n = len(maze[0])
+    return 0<= x and x<m and 0<=y and y<n and maze[x][y] == 0
+
+def finding_path(maze, step, current, stop):   
+    global min
+    maze[current[0]][current[1]] = 1
+    if current == stop:
+        if min == None:
+            min = step
+        else:
+            min = step if min > step else min
+    else:
+        x = current[0]
+        y = current[1]
+        if valid_move(maze, (x+1, y)):
+            finding_path(maze, step+1, (x+1, y),stop)
+        if valid_move(maze, (x-1, y)):
+            finding_path(maze, step+1, (x-1, y),stop)
+        if valid_move(maze, (x, y+1)):
+            finding_path(maze, step+1, (x, y+1),stop)
+        if valid_move(maze, (x, y-1)):
+            finding_path(maze, step+1, (x, y-1),stop)
+
+start = (3,0)
+stop = (0,0)
+
+finding_path(maze, 0, start,stop)
+
+print(min)
+
+
